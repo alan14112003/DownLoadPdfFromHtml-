b12c70e59a6fdbaeef21417cea29a8f35a82d334
alan14112003 <sonnn.21it@gmil.com>
2023-08-14 18:27:25 +0700

    Change some thing

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const multer = require("multer");
 const upload = multer({ dest: "src/uploads/" });
 const cors = require("cors");
 const fs = require("fs");
+const url = require("url");
 
 function isValidUrl(input = "") {
   return !!input.includes("://elib.vku.udn.vn:8080/ViewPDFOnline/document.php");
@@ -13,6 +14,8 @@ function isValidUrl(input = "") {
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + "/public"));
 
 app.use(
   cors({
@@ -29,18 +32,22 @@ app.post(
   "/images-to-pdf",
   upload.array("images", 10000),
   async (req, res, next) => {
+    const currentUrl = url.parse(req.url);
+    const currentDomain = currentUrl.host;
+
     const images = req.files;
     const doc = new PDFDocument({ size: "A4" });
-    doc.pipe(fs.createWriteStream("./output.pdf"));
+    doc.pipe(fs.createWriteStream("./public/output.pdf"));
     for (const image of images) {
       doc.image(image.path);
       doc.addPage({ size: "A4" });
     }
+    doc.end();
 
     return res.json({
       status: false,
       message: "url hợp lệ",
-      body: images,
+      body: currentDomain + "/public/output.pdf",
     });
   }
 );
