09c3a31de77dd38d80db55c53c88a8e53148ae1e
alan14112003 <sonnn.21it@gmil.com>
2023-08-14 17:51:01 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,46 @@
+const PDFDocument = require("pdfkit");
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "src/uploads/" });
+const cors = require("cors");
+const fs = require("fs");
+
+function isValidUrl(input = "") {
+  return !!input.includes("://elib.vku.udn.vn:8080/ViewPDFOnline/document.php");
+}
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  return res.render("index");
+});
+
+app.post(
+  "/images-to-pdf",
+  upload.array("images", 10000),
+  async (req, res, next) => {
+    const images = req.files;
+    const doc = new PDFDocument({ size: "A4" });
+    doc.pipe(fs.createWriteStream("/src/output.pdf"));
+    for (const image of images) {
+      // doc.image()
+      console.log(image);
+    }
+
+    return res.json({
+      status: false,
+      message: "url hợp lệ",
+      body: images,
+    });
+  }
+);
+
+app.listen(80, () => {
+  console.log(`app đang chạy ở cổng 80`);
+});
