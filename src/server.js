f7f678f2032b59e275b47a489697dc99f470466d
alan14112003 <sonnn.21it@gmil.com>
2023-08-15 17:18:39 +0700

    fix dir

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "uploads");
+      cb(null, "src/uploads");
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname);
