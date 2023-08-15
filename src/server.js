c647c7bb75bd61bee940b81ac6a7c1ee4e02c248
alan14112003 <sonnn.21it@gmil.com>
2023-08-15 17:44:24 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "/uploads");
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+  // filename: function (req, file, cb) {
+  //   cb(null, file.originalname);
+  // },
 });
 const upload = multer({
   storage: storage,
