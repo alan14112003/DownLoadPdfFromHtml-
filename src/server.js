a2327194a06785239678e68170a88bf12d630e41
alan14112003 <sonnn.21it@gmil.com>
2023-08-15 17:14:23 +0700

    fix

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const upload = multer({
       cb(null, "uploads");
     },
     filename: function (req, file, cb) {
-      cb(null, file.fileOriginalName);
+      cb(null, file.originalname);
     },
   }),
 });
